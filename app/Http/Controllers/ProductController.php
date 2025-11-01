0ddf84b55825242c16c387bd818f94d6deb379de
Lisa Haven <lisahaven2008@gmail.com>
2025-11-01 21:48:47 +0100

    Product controller aangepast

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,89 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
-
 class ProductController extends Controller
 {
     private $productModel;
 
     public function __construct()
     {
-       $this->productModel = new ProductModel();
+        $this->productModel = new ProductModel();
     }
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $products = $this->productModel->sp_GetAllProducts();
+        $products = $this->productModel->sp_GetProducts();
 
-        return view('product.index', [
+        return view('products.index', [
             'title' => 'Overzicht Magazijn Jamin',
             'products' => $products
         ]);
-    }
-
-    public function allergenenInfo()
-    {
-        return view('product.allergeenInfo', [
-            'title' => 'Allergeen Informatie'
-        ]);
-    }
-
-     public function leverantieInfo()
-    {
-        return view('product.leverantieInfo', [
-            'title' => 'Leverantie Informatie'
-        ]);
-    }
-
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProductModel $productModel)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ProductModel $productModel)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, ProductModel $productModel)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(ProductModel $productModel)
-    {
-        //
     }
 }
